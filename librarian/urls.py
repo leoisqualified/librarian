a3fadb2b8317feea0bdd1fb8371cb3e3fc84c5d5
leoisqualified <eaadenutsi@st.ug.edu.gh>
2025-02-21 11:38:11 +0000

    added urls.py

--- librarian/urls.py
+++ librarian/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from . import views
 
-
-urlspatterns = [
-    path('book_list/', views.book_list, name='book_list'),
-    path('add_book/', views.add_book, name='add_book'),
-    path('edit_book/<int:request_id>/', views.edit_book, name='edit_book'),
+urlpatterns = [
+    path('books/', views.book_list, name='book_list'),
+    path('books/add/', views.add_book, name='add_book'),
+    path('books/edit/<int:book_id>/', views.edit_book, name='edit_book'),
+    path('books/delete/<int:book_id>/', views.delete_book, name='delete_book'),
+    path('material-types/', views.material_type_list, name='material_type_list'),
+    path('material-types/add/', views.add_material_type, name='add_material_type'),
+    path('material-types/edit/<int:material_type_id>/', views.edit_material_type, name='edit_material_type'),
+    path('material-types/delete/<int:material_type_id>/', views.delete_material_type, name='delete_material_type'),
 ]
